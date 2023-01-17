b5ff54d839e0a67e2b8c87d52983c21b2af20aa9
satyazmx <thakursatya6@gmail.com>
2023-01-17 12:14:45 +0530

    add logger & exception module

---
+++ demo.py
@@ -0,0 +1,14 @@
+import sys
+from src.forest.exception import CustomException
+
+def test():
+    try:
+        x = 1/0
+    except Exception as e:
+        raise CustomException(e,sys)
+
+if __name__ == '__main__':
+    try:
+        test()
+    except Exception as e:
+        print(e)
---
+++ src/forest/logger/__init__.py
@@ -0,0 +1,19 @@
+import os
+import logging
+from from_root import from_root
+from datetime import datetime
+
+LOG_FILE_NAME = f"{datetime.now().strftime('%m%d%Y_%M%H%S')}.log"
+
+LOG_DIR = 'logs'
+
+os.makedirs(LOG_DIR,exist_ok=True)
+
+LOG_FILE_PATH = os.path.join(LOG_DIR,LOG_FILE_NAME)
+
+logging.basicConfig(
+    filemode='w',
+    filename= LOG_FILE_PATH,
+    format="[%(asctime)s] %(lineno)d - %(levelname)s %(messages)s",
+    level=logging.INFO
+)
