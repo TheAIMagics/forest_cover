0e9ea6b4193870f5b58fc0b18e8daeb8450343a6
satyazmx <thakursatya6@gmail.com>
2023-01-27 09:59:04 +0530

    AWS S3 operations

--- src/forest/components/model_training.py
+++ src/forest/components/model_training.py
@@ -7,22 +7,53 @@ from src.forest.entity.artifact_entity import *
 from neuro_mf  import ModelFactory
 from src.forest.entity.estimator import ForestModel
 from src.forest.utils.main_utils import *
+from typing import List, Tuple
+from sklearn.metrics import f1_score, precision_score, recall_score
 
 class ModelTrainer:
     def __init__(self, data_transformation_artifact: DataTransformationArtifact,
                  model_trainer_config: ModelTrainerConfig):
         self.data_transformation_artifact = data_transformation_artifact
         self.model_trainer_config = model_trainer_config
+
+    def get_model_object_and_report(self, train: np.array, test: np.array) -> Tuple[object, object]:
+        """
+        Method Name :   get_model_object_and_report
+        Description :   This function uses neuro_mf to get the best model object and report of the best model
+        
+        Output      :   Returns metric artifact object and best model object
+        On Failure  :   Write an exception log and then raise an exception
+        """
+        try:
+            logging.info("Using neuro_mf to get best model object and report")
+            model_factory = ModelFactory(model_config_path=self.model_trainer_config.model_config_file_path)
+            
+            x_train, y_train, x_test, y_test = train[:, :-1], train[:, -1], test[:, :-1], test[:, -1]
+
+            best_model_detail = model_factory.get_best_model(
+                X=x_train,y=y_train,base_accuracy=self.model_trainer_config.expected_accuracy
+            )
+            model_obj = best_model_detail.best_model
+
+            y_pred = model_obj.predict(x_test)
+            
+            f1 = f1_score(y_test, y_pred, average='micro')  
+            precision = precision_score(y_test, y_pred, average='micro')  
+            recall = recall_score(y_test, y_pred, average='micro')
+            metric_artifact = ClassificationMetricArtifact(f1_score=f1, precision_score=precision, recall_score=recall)
+            
+            return best_model_detail, metric_artifact
+        
+        except Exception as e:
+            raise CustomException(e, sys) from e
     
     def initiate_model_trainer(self) -> ModelTrainerArtifact:
         logging.info("Entered initiate_model_trainer method of ModelTrainer class")
         try:
             train_arr = load_numpy_array_data(file_path=self.data_transformation_artifact.transformed_train_file_path)
             test_arr = load_numpy_array_data(file_path=self.data_transformation_artifact.transformed_test_file_path)
-            x_train, y_train, x_test, y_test = train_arr[:, :-1], train_arr[:, -1], test_arr[:, :-1], test_arr[:, -1]
-
-            model_factory = ModelFactory(model_config_path=self.model_trainer_config.model_config_file_path)
-            best_model_detail = model_factory.get_best_model(X=x_train,y=y_train,base_accuracy=self.model_trainer_config.expected_accuracy)
+            
+            best_model_detail ,metric_artifact = self.get_model_object_and_report(train=train_arr, test=test_arr)
             
             if best_model_detail.best_score < self.model_trainer_config.expected_accuracy:
                 logging.info("No best model found with score more than base score")
@@ -36,7 +67,6 @@ class ModelTrainer:
             logging.info("Created best model file path.")
             save_object(self.model_trainer_config.trained_model_file_path, forest_model)
 
-            metric_artifact = ClassificationMetricArtifact(f1_score=0.8, precision_score=0.8, recall_score=0.9)
             model_trainer_artifact = ModelTrainerArtifact(
                 trained_model_file_path=self.model_trainer_config.trained_model_file_path,
                 metric_artifact=metric_artifact,
